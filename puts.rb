e4393c044eb66a59c6d2d935802b52c5ac4ddffd
Jordan Bailey <jordanbailey201@gmail.com>
2018-09-21 09:00:07 +0000

    learn ruby the hard way part 1

---
+++ puts.rb
@@ -0,0 +1,15 @@
+puts "hello world"
+puts "hello again"
+puts "i like typing this"
+puts "this is fun"
+puts "yay printing"
+puts "id much rather you 'not'"
+puts 'I "said" do not touch this'
+
+#puts "hello world"
+#puts "hello again"
+#puts "i like typing this"
+puts "this is fun"
+#puts "yay printing"
+#puts "id much rather you 'not'"
+#puts 'I "said" do not touch this'
